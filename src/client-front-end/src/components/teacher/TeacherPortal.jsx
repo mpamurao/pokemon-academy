19f12278d26df87b45d4d1c63c2fec8bf070288a
Marie Amurao <mamurao@g.clemson.edu>
2021-06-21 16:19:25 -0400

    teacher portal navbar menu drop down

--- src/client-front-end/src/components/teacher/TeacherPortal.jsx
+++ src/client-front-end/src/components/teacher/TeacherPortal.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PortalNavBar from '../TeacherPortalNavBar';
 
 class TeacherPortal extends Component {
     constructor(props) {
@@ -22,6 +23,7 @@ class TeacherPortal extends Component {
         return (
             <div>
                 TEACHER PORTAL
+                <PortalNavBar />
             </div>
         );
     }
