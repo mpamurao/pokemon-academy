ef4747b2978c9fc7549d76ea7872ce64728129e9
Marie Amurao <mamurao@g.clemson.edu>
2021-06-29 01:05:14 -0400

    edite student controller

--- src/main/java/classPortal/controller/StudentController.java
+++ src/main/java/classPortal/controller/StudentController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import classPortal.model.StudentModel;
-import classPortal.model.TeacherModel;
 import classPortal.service.StudentService;
 
 @RestController
